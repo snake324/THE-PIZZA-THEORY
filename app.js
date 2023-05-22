deb2c9517892147026f40e5f42df639ed638a20b
snake324 <kristi4n90@gmail.com>
2023-05-22 12:48:41 +0200

    pasos pizza

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
-const mass= "Masa de pizza congelada"
+const dought= "Masa de pizza congelada"
+const tomatosauce= "Salsa de tomate"
+const mozzarella= "Mozzarella"
+const hamyork= "Jamón York"
+const tuna= "Atún"
 
 function start(){
 
-    buyDought()
+    defrostDought()
+    strechtDought()
+    puttomatosauce()
+    spreadtomatosauce()
+    putmozzarellaonpizza()
+    puthamyorkonpizza()
+    puttunaonpizza()
+    putpizzaintheoven()
+    takepizzaoutofoven()
+    slicepizza()
+    deliverpizza()
+
 }
 
-function buyDought() {
-    console.log("masa comprada")
+function defrostDought() {
+    console.log("Descongelar masa")
+}
+function strechtDought() {
+    console.log("Estirar masa")
+}
+function puttomatosauce() {
+    console.log("Poner salsa de tomate en la masa")
+}
+function spreadtomatosauce() {
+    console.log("Extender salsa de tomate")
+}
+function putmozzarellaonpizza() {
+    console.log("Poner mozzarella en la pizza")
+}
+function puthamyorkonpizza() {
+    console.log("Poner jamón york en la pizza")
+}
+function puttunaonpizza() {
+    console.log("Poner atún en la pizza")
+}
+function putpizzaintheoven() {
+    console.log("Poner pizza en el horno 10 minutos")
+}
+function takepizzaoutofoven() {
+    console.log("Sacar la pizza del horno")
+}
+function slicepizza() {
+    console.log("Cortar pizza en porciones")
+}
+function deliverpizza() {
+    console.log("Entregar pizza")
 }
 
 start()
